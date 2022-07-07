09eb6a2436d9446fa1ddce2341b0a0b7cb5581dc
bela <you@example.com>
2022-07-07 12:09:06 -0700

    hello world project done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - entry
+ *
+ *return - return 0  always true
+ */
+int main(void)
+{
+  printf("with proper grammer, but the outcome is a piece of art,\n");
+  return (0);
+}
